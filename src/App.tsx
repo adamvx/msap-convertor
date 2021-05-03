200b1c75f544c2aab3fd59910a0758bc2794cc86
Adam Vician <adamvician@gmail.com>
2021-05-03 13:05:50 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { convert } from 'exchange-rates-api'
 import './App.css';
 
 function App() {
+
+  const [eur, setEur] = useState<string>()
+  const [usd, setUsd] = useState<string>()
+
+  const onPress = async () => {
+    if (!eur) return;
+    const amount = await convert(parseInt(eur), 'EUR', 'USD', new Date());
+    setUsd(amount.toString())
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <body className="body">
+        <h1>Convertor</h1>
+        <p>Menova kalkulacka co vam premeni EUR na USD.</p>
+        <p>Menime len EUR na USD a nie naopak lebo som bol lenivy.</p>
+        <div style={{ flexDirection: 'row', marginTop: 32 }}>
+          <input value={eur} onChange={e => setEur(e.target.value)} placeholder='EUR' style={{ marginRight: 32 }}></input>
+          <button onClick={onPress}>CONVERT</button>
+          <input value={usd} contentEditable={false} placeholder='USD' style={{ marginLeft: 32 }}></input>
+        </div>
+      </body>
     </div>
   );
 }
