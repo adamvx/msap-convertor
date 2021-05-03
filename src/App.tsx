5497824aba096ae2c169b344886878a25e70aa12
Adam Vician <adamvician@gmail.com>
2021-05-03 18:09:39 +0200

    Minor layout chnages

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,56 @@
-import React, { useState } from 'react';
-import { convert } from 'exchange-rates-api'
+import { exchangeRates } from 'exchange-rates-api';
+import moment from 'moment';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
 
+  const [rate, setRate] = useState<number>()
   const [eur, setEur] = useState<string>()
   const [usd, setUsd] = useState<string>()
 
-  const onPress = async () => {
-    if (!eur) return;
-    const amount = await convert(parseInt(eur), 'EUR', 'USD', new Date());
-    setUsd(amount.toString())
+  useEffect(() => {
+    exchangeRates().latest().symbols('USD').fetch().then(res => {
+      if (typeof res === "number") {
+        setRate(res)
+      }
+    });
+  }, [])
+
+  if (!rate) {
+    return (
+      <div className="app">
+        <body className="body">
+
+        </body>
+      </div>
+    )
+  }
+
+  const onEurChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEur(e.target.value)
+    const val = parseInt(e.target.value) * rate
+    setUsd(isNaN(val) ? "" : val.toString())
+  }
+
+  const onUsdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsd(e.target.value)
+    console.log(e.target.value)
+    const val = parseInt(e.target.value) / rate
+    setEur(isNaN(val) ? "" : val.toString())
   }
 
   return (
     <div className="app">
       <body className="body">
         <h1>Convertor</h1>
-        <p>Menova kalkulacka co vam premeni EUR na USD.</p>
-        <p>Menime len EUR na USD a nie naopak lebo som bol lenivy.</p>
-        <div style={{ flexDirection: 'row', marginTop: 32 }}>
-          <input value={eur} onChange={e => setEur(e.target.value)} placeholder='EUR' style={{ marginRight: 32 }}></input>
-          <button onClick={onPress}>CONVERT</button>
-          <input value={usd} contentEditable={false} placeholder='USD' style={{ marginLeft: 32 }}></input>
+        <p style={{ marginBottom: 48 }}>{`Kurz k dnu ${moment().format('DD.MM.YYYY')} je 1 EUR = ${rate} USD`}</p>
+        <div style={{ flexDirection: 'row', marginBottom: 48 }}>
+          <input className='appInput' value={eur} onChange={onEurChange} placeholder='EUR' style={{ marginRight: 32 }}></input>
+          <span>to</span>
+          <input className='appInput' value={usd} onChange={onUsdChange} placeholder='USD' style={{ marginLeft: 32 }}></input>
         </div>
+        <p style={{ fontSize: 16, maxWidth: '450px' }}>Toto je mala menova kalkulacka co vam premeni EUR na USD. Pri obnoveni stranky sa nacita novy kurz.</p>
       </body>
     </div>
   );
